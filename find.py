da792161b38ae5171f68f30c74f02fb4afbbed4b
Sleyter Monteiro <87656234+sleyter-monteiro@users.noreply.github.com>
2024-03-26 12:13:50 +0100

    code

--- find.py
+++ find.py
@@ -1,19 +1,27 @@
-# Recherche de mots-clés dans un fichier DLT (.dlt)
+import os
+
+# Fonction de recherche de mots-clés dans un fichier DLT (.dlt)
 import re
 
 def find_keyword_in_file(file_path, keyword):
     with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
         content = file.read()
-    matches = {keyword: len(re.finditer(keyword, content))}
+    matches = {keyword: sum(1 for _ in re.finditer(keyword, content))}
     return matches
 
-# Utilisation de la fonction
-keyword = input("Entrez le mot clé à rechercher : ") # ["IMEI", "IOS", "Name", "Serial", "MAC", "IMEI 2", "ICCID"]
-matches = find_keyword_in_file('log_132846.dlt', keyword)
+# Obtention de la liste des fichiers DLT dans le dossier du script
+folder_path = '.' 
+dlt_files = [f for f in os.listdir(folder_path) if f.endswith('.dlt')]
+
+# Utilisation de la fonction pour chaque fichier DLT
+keyword = input("Entrez le mot clé à rechercher : ")
 print("Résultats de la recherche :")
-print("-----------------------------------")
-print("| {:<10} | {:<10} |".format("Mot-clé", "Résultat"))
-print("-----------------------------------")
-for keyword, count in matches.items():
-    print("| {:<10} | {:<10} |".format(keyword, count))
-print("-----------------------------------")
+print("---------------------------------------------")
+print("| {:<40} | {:<10} |".format("Fichier", "Résultats"))
+print("---------------------------------------------")
+for dlt_file in dlt_files:
+    file_path = os.path.join(folder_path, dlt_file)
+    matches = find_keyword_in_file(file_path, keyword)
+    for keyword, count in matches.items():
+        print("| {:<40} | {:<10} |".format(dlt_file, count))
+print("---------------------------------------------")
